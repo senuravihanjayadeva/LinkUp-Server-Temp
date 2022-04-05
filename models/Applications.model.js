e5b68bb65942505b31fadbc9ce14fb919fe22075
senuravihanjayadeva <senurajayadeva@gmail.com>
2022-04-05 19:42:13 +0530

    Add userId to application model

--- models/Applications.model.js
+++ models/Applications.model.js
@@ -1,19 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ApplicationsSchema = new Schema(
-	{
-		applicantName: String,
-		nic: String,
-		contactNumber: String,
-		university: String,
-		skills: String,
-		languages: String,
-		linkedIn: String,
-		github: String,
-		status: String,
-	},
-	{ timestamps: true }
+  {
+    applicantName: String,
+    nic: String,
+    contactNumber: String,
+    university: String,
+    skills: String,
+    languages: String,
+    linkedIn: String,
+    github: String,
+    status: String,
+    userId: mongoose.Types.ObjectId,
+  },
+  { timestamps: true }
 );
 
-module.exports = Application = mongoose.model("Application", ApplicationsSchema);
+module.exports = Application = mongoose.model(
+  "Application",
+  ApplicationsSchema
+);
